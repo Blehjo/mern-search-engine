8fd026dbcefb92362203013ee6a9d345efff8ca5
bseton11-YJKnqQ <bseton11@gmail.com>
2022-03-17 03:21:38 -0500

    created mutation.js and added exports

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+  mutation createUser($usernname: String!, $email: String!, $password: String!) {
+    addProfile(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: ID!, $savedBook: String!) {
+    savedBook(userId: $userId, savedBook: $savedBook) {
+      _id
+      username
+      savedBook
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation deletebook($savedBook: String!) {
+    deletebook(skill: $savedBook) {
+      _id
+      name
+      savedBook
+    }
+  }
+`;
